3a823f5fddf54b9b4a16299d4165d6f535626ec0
Steve <stg7@gmx.de>
2015-09-30 21:59:47 +0200

    add whatismyip wrapper

---
+++ webapis/whatismyip/whatismyip.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+    Api Wrapper for whatismyip queries
+
+    Copyright 2015-today
+
+    Author: Steve Göring
+    Contact: stg7@gmx.de
+"""
+
+import sys
+import os
+import argparse
+import json
+import urllib.request
+import shelve
+import re
+
+sys.path.insert(0, os.path.dirname(__file__) + '/..')
+from webutils import CachedRequester
+from webutils import lInfo
+
+
+class Whatismyip(CachedRequester):
+    """
+
+    """
+    _baseurl = "http://whatismyip.org/"
+
+    def get(self):
+        res = self._raw_request({})
+        return re.findall("[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+", res)[0]
+
+
+def main(params):
+    whatismyip = Whatismyip()
+    print(json.dumps(whatismyip.get(), indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
